6061a6d26c43bc18c6960dac25bc9c46efaa7444
Hema-m-01 <140885541+Hema-m-01@users.noreply.github.com>
2023-08-31 19:17:00 +0530

    Create dec3x8.v

---
+++ dec3x8/dec3x8.v
@@ -0,0 +1,19 @@
+module dec3x8(output reg[7:0]y,input [2:0]i,input en);
+always@(*)
+begin
+if(!en)
+y=8'b0;
+else
+case({i})
+3'b000:y=8'b0000_0001;
+3'b001:y=8'b0000_0010;
+3'b010:y=8'b0000_0100;
+3'b011:y=8'b0000_1000;
+3'b100:y=8'b0001_0000;
+3'b101:y=8'b0010_0000;
+3'b110:y=8'b0100_0000;
+3'b111:y=8'b1000_0000;
+default:y='bx;
+endcase
+end
+endmodule
